65a257cbeb8fda7614921a22366dc391dc19ae0a
guney <supurjsgml1@naver.com>
2026-08-25 22:27:11 +0900

    구라파나 파드 분리용 변경

--- server/api/grafana/iframe-src.ts
+++ server/api/grafana/iframe-src.ts
@@ -3,7 +3,7 @@ export default defineEventHandler((event) => {
   const type = (query.type as string) || 'batch'
 
   if (type === 'api') {
-    return '/grafana/public-dashboards/c21841ab9858481eb765bb262c3ba3e2?from=now-24h&to=now&timezone=browser&refresh=10s'
+    return '/grafana/public-dashboards/8b13cbddd7b34084ac9cf50a0524ce50?from=now-24h&to=now&timezone=browser&refresh=10s'
   }
   return '/grafana/public-dashboards/6db3adcb8b00421589797ad121289dd1?from=now-24h&to=now&timezone=browser&refresh=10s'
 })
